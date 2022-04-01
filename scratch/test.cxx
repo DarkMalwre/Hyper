7b5edbd1efe6dc78d34c3177334e8c7b2c01d1eb
Rayyan (r - eye - an) <xfaonae@protonmail.com>
2022-04-01 00:02:10 -0400

    Color support

--- scratch/test.cxx
+++ scratch/test.cxx
@@ -6,7 +6,7 @@ using namespace std;
 using namespace Hyper;
 
 int main() {
-    cout << Terminal::Colorize("Hello There :Red:", "red");
+    cout << Terminal::Colorize("Hello There :Red:", "red;green");
 
     return 0;
 }
