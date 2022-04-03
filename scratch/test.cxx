95bf0bb9f7dcad9eede1580f5045d49258a367eb
XFaonAE <xfaonae@protonmail.com>
2022-04-03 01:10:18 -0400

    refactor(*): Line

--- scratch/test.cxx
+++ scratch/test.cxx
@@ -1,14 +1,13 @@
 #include <Hyper/Terminal.h>
 #include <Hyper/JSON.h>
 #include <Hyper/StringUtil.h>
-
 #include <iostream>
 
 using namespace std;
 using namespace Hyper;
 
 int main() {
-    cout << Terminal::Colorize("Hello There :Red:", "red;green");
+    cout << Terminal::Colorize("Hello There :Red:", "red");
 
     return 0;
 }
